d245a8cbd055abff2f24ac05e581fd9797e872f8
Rita Cheng <rita@ritacheng.com>
2015-04-20 22:47:07 -0700

    updates for deployment to heroku

--- project/build.scala
+++ project/build.scala
@@ -4,6 +4,9 @@ import org.scalatra.sbt._
 import org.scalatra.sbt.PluginKeys._
 import com.mojolly.scalate.ScalatePlugin._
 import ScalateKeys._
+import com.heroku.sbt.HerokuPlugin
+import com.heroku.sbt.HerokuPlugin.autoImport._
+import com.typesafe.sbt.packager.archetypes._
 
 object KaybotBuild extends Build {
   val Organization = "com.github.metermaid"
@@ -27,10 +30,11 @@ object KaybotBuild extends Build {
         "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "9.1.5.v20140505" % "container",
-        "org.eclipse.jetty" % "jetty-plus" % "9.1.5.v20140505" % "container",
+        "org.eclipse.jetty" % "jetty-webapp" % "9.1.5.v20140505" % "compile;container",
+        "org.eclipse.jetty" % "jetty-plus" % "9.1.5.v20140505" % "compile;container",
         "javax.servlet" % "javax.servlet-api" % "3.1.0"
       ),
+      herokuAppName in Compile := "kaybot",
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(
           TemplateConfig(
@@ -44,5 +48,5 @@ object KaybotBuild extends Build {
         )
       }
     )
-  )
+  ).enablePlugins(JavaAppPackaging)
 }
